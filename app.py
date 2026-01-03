049f1c4dded8a0f25bde91b364cef128cb0c6f41
DinidiGallage <dinidisg@gmail.com>
2026-01-03 15:36:48 +0530

    updated

--- app.py
+++ app.py
@@ -9,7 +9,3 @@ app.include_router(prediction_router, prefix="/api")
 app.include_router(cattle_disease_router, prefix="/api")
 
 
-app = FastAPI()
-
-app.include_router(prediction_router, prefix="/api")
-
