d64049a9c777d8e8bc93b374dd6102b6162a14c3
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2026-04-18 12:33:21 +0530

    path

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 SECRET_KEY = "your-super-secret-keygggggggggggggggggggg"
-MODEL_PATH_RF = "Model/milk_yield_rf_v5.pickle"
+
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH_RF = os.path.join(BASE_DIR, "Model", "milk_yield_rf_v5.pickle")
 MODEL_PATH = os.path.join(BASE_DIR, "AI_Predictor", "pregnancy_model.pkl")
 COX_MODEL_PATH = os.path.join(BASE_DIR, "AI_Predictor", "cox_model.pkl") 
